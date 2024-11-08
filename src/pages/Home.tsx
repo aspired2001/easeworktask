01212b3b67932c0d2230f5c424fe61e2d2698ea9
aspired2001 <100749624+aspired2001@users.noreply.github.com>
2024-11-08 10:59:26 +0530

    Some changes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,10 @@
 export function Home() {
-  return <h1>Home</h1>
+  return (
+    <div className="container">
+      <h1 className="text-center" style={{
+        fontSize:"2.3rem"
+      }}>Welcome to the Home Page</h1>
+      <h5 className="text-center text-muted">Click On Store to navigate to product page</h5>
+    </div>
+  )
 }
